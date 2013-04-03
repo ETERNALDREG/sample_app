299d80dc89e99cf489c8603e08280e0c4c83bf0c
ETERNALDREG <dregfromthelight@i.ua>
2013-04-03 17:14:51 +0300

    prosta bog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e5abb0a7a7be57182921e0d40c6e7c8906366a631c52d40a9a6f2464c5efe221b5a590519d4cabb34a5ee6af39225d90cbfc4efb48b24efe9860dddc366e97a0'
